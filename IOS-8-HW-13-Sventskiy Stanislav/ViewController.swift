c0cde1f6d955e311507a788678aa5a8944af91d2
StanislavSSY <stanislav.mifub@yandex.ru>
2023-03-31 16:41:11 +0700

    Initial Commit

---
+++ IOS-8-HW-13-Sventskiy Stanislav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-8-HW-13-Sventskiy Stanislav
+//
+//  Created by Stanislav Stanislav on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
